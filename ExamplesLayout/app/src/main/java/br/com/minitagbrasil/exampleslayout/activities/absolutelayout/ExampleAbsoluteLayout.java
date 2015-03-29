6e7c04755d718fc9c5b8ed3423bd6244fc5250a2
Gabriel A. Garcia <ggarcia.java@gmail.com>
2015-03-29 02:07:56 -0300

    Add example with AbsoluteLayout (deprecated)

---
+++ ExamplesLayout/app/src/main/java/br/com/minitagbrasil/exampleslayout/activities/absolutelayout/ExampleAbsoluteLayout.java
@@ -0,0 +1,18 @@
+package br.com.minitagbrasil.exampleslayout.activities.absolutelayout;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import br.com.minitagbrasil.exampleslayout.R;
+
+/**
+ * Created by ggarcia on 29/03/15.
+ */
+public class ExampleAbsoluteLayout extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_absolutelayout);
+    }
+}
