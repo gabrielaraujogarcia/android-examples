e45578d206341efe2c5ff32fe0c8c54ca7966599
Gabriel A. Garcia <ggarcia.java@gmail.com>
2015-04-08 00:13:22 -0300

    Added example of using your custom resources

---
+++ ExampleViewAPI/app/src/main/java/br/com/minitagbrasil/exampleviewapi/ExampleCustonResources.java
@@ -0,0 +1,16 @@
+package br.com.minitagbrasil.exampleviewapi;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by ggarcia on 08/04/15.
+ */
+public class ExampleCustonResources extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.custom_resources);
+    }
+}
